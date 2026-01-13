037bafdec49634bed99011fd662ebfbad4abe213
Preethi0716 <mercylinpreethi@gmail.com>
2026-01-13 15:51:08 +0530

    Added a wrapper function

---
+++ wrapper/build/CMakeFiles/aom_wrapper.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aom_wrapper.dir/aom_wrapper.cpp.obj"
+  "CMakeFiles/aom_wrapper.dir/aom_wrapper.cpp.obj.d"
+  "aom_wrapper.dll"
+  "aom_wrapper.dll.manifest"
+  "aom_wrapper.lib"
+  "aom_wrapper.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aom_wrapper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
